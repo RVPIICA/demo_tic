c7409500dde186fde4ea68a9b2222b371ed11b75
RVargas <randall.vargas@iica.int>
2022-11-08 10:31:09 -0600

    Elimino codigo no usado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import Bienvenida from './components/Bienvenida';
-import HolaMundo from './components/HolaMundo';
-import HolaMundoNombre from './components/HolaMundoNombre';
-import logo from './logo.svg';
 import { DefaultRouter } from './routes/router';
 
 function App() {
